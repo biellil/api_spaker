18393d4dc955dea3bb9c73437e87afaf1c882e46
biel.lil <78098857+biellil@users.noreply.github.com>
2023-05-05 16:03:53 -0300

    05/05

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
+import { Rcon } from "rcon-js";
 
-function App() {
+const SERVER_IP = "127.0.0.1"; // IP do servidor Minecraft
+const SERVER_PORT = 25575; // porta rcon do servidor Minecraft
+const RCON_PASSWORD = "senha_rcon"; // senha rcon do servidor Minecraft
+
+const socket = io(`http://${SERVER_IP}:${SERVER_PORT}`);
+
+const MinecraftServerInfo: React.FC = () => {
+  const [players, setPlayers] = useState<string[]>([]);
+  const [tps, setTps] = useState<number>(0);
+
+  useEffect(() => {
+    const rcon = new Rcon(SERVER_IP, SERVER_PORT, RCON_PASSWORD);
+
+    const getServerInfo = async () => {
+      try {
+        await rcon.connect();
+        const [tpsResponse, playersResponse] = await Promise.all([
+          rcon.send("tps"),
+          rcon.send("list"),
+        ]);
+        const tps = parseFloat(tpsResponse.split(": ")[1]);
+        const players = playersResponse.split(": ")[1].split(", ");
+        setTps(tps);
+        setPlayers(players);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        rcon.end();
+      }
+    };
+
+    getServerInfo();
+    const interval = setInterval(getServerInfo, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Desempenho do Servidor:</h2>
+      <p>TPS: {tps.toFixed(2)}</p>
+      <p>Jogadores: {players.join(", ")}</p>
     </div>
   );
-}
+};
 
-export default App;
+export default MinecraftServerInfo;
