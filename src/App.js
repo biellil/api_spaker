1f25346e67116d0d836717c9c4b22c182881bd78
biel.lil <78098857+biellil@users.noreply.github.com>
2023-05-05 13:06:57 -0300

    05/05

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [statistics, setStatistics] = useState({});
+
+  useEffect(() => {
+    const fetchStatistics = async () => {
+      const response = await axios.get('/api/spark/statistics');
+      setStatistics(response.data);
+    };
+    fetchStatistics();
+    const intervalId = setInterval(fetchStatistics, 5000); // atualiza as estatísticas a cada 5 segundos
+    return () => clearInterval(intervalId); // limpa o intervalo quando o componente é desmontado
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Spark Statistics</h1>
+      <p>TPS: {statistics.tps}</p>
+      <p>MSPT Mean: {statistics.msptMean}</p>
+      <p>MSPT 95th Percentile: {statistics.mspt95Percentile}</p>
+      <p>CPU Usage: {statistics.cpuUsage}</p>
+      <h2>Garbage Collectors:</h2>
+      <ul>
+        {Object.keys(statistics.gc).map((gcName) => (
+          <li key={gcName}>
+            {gcName}: avgFrequency={statistics.gc[gcName].avgFrequency}, avgTime={statistics.gc[gcName].avgTime}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
